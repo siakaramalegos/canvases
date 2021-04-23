8aa489ca4d2bdeaede34b1291f17046c8e18e97e
Sia Karamalegos <siakaramalegos@gmail.com>
2021-04-23 14:55:25 -0500

    Reuse generate images

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const images = [{
-  id: 1862,
-  label: "Horizontal",
-}, {
-  id: 5191,
-  label: "Vertical",
-}, {
-  id: 3451,
-  label: "Square-ish"
-}]
+function generateImgs(targetNode, transforms) {
+  const baseUrl = "https://res.cloudinary.com/tedsvintageart/image/upload"
+  const folder = "Maps"
+  const images = [{
+    id: 1862,
+    label: "Horizontal",
+  }, {
+    id: 5191,
+    label: "Vertical",
+  }, {
+    id: 3451,
+    label: "Square-ish"
+  }]
+
+  images.forEach(image => {
+    const h2 = document.createElement("h2")
+    h2.innerText = image.label
+    targetNode.appendChild(h2)
+
+    const img = document.createElement("img")
+    img.src = `${baseUrl}/${transforms}/${folder}/${image.id}.jpg`
+    targetNode.appendChild(img)
+  })
+}
 
 const base = document.querySelector("#base")
-const baseUrl = "https://res.cloudinary.com/tedsvintageart/image/upload"
-const folder = "Maps"
-
-images.forEach(image => {
-  const h2 = document.createElement("h2")
-  h2.innerText = image.label
-  base.appendChild(h2)
-
-  const img = document.createElement("img")
-  img.src = `${baseUrl}/w_400/${folder}/${image.id}.jpg`
-  base.appendChild(img)
-})
+generateImgs(base, "w_400")
